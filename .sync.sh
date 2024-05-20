dd7eebf5c0812fa4c8e244bcc2b256e61d33d0eb
khalidlife <khalidlife@163.com>
2024-05-20 15:30:19 +0800

    🎉 init(base,types): 项目初始化

---
+++ .sync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cnpm sync @elog-muyao/cli
+cnpm sync @elog-muyao/core
+cnpm sync @elog-muyao/deploy
+cnpm sync @elog-muyao/plugin-adapter
+cnpm sync @elog-muyao/plugin-image
+cnpm sync @elog-muyao/sdk-confluence
+cnpm sync @elog-muyao/sdk-flowus
+cnpm sync @elog-muyao/sdk-notion
+cnpm sync @elog-muyao/sdk-yuque
+cnpm sync @elog-muyao/sdk-wordpress
+cnpm sync @elog-muyao/sdk-feishu
+cnpm sync @elog-muyao/shared
+cnpm sync @elog-muyao/types
---
+++ packages/types/README.md
@@ -0,0 +1,11 @@
+# `@elog-pro/types`
+
+> TODO: description
+
+## Usage
+
+```
+const types = require('@elog-pro/types');
+
+// TODO: DEMONSTRATE API
+```
